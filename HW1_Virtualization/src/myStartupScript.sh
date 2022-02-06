04021653a30ee9b1a5b042b7a41d3f67a10b7f7e
Marco Tulio <mruizq@unah.hn>
2022-02-06 16:55:42 -0600

    Actualizacion->HW1

--- HW1_Virtualization/src/myStartupScript.sh
+++ HW1_Virtualization/src/myStartupScript.sh
@@ -1,3 +1,6 @@
-cd /var/www/html/blog
+apt-get install git
+cd /var/www/html
+git clone https://github.com/martulioruiz/martulioruiz.github.io.git
+cd "martulioruiz.github.io"
 bundle install
 bundle exec jekyll serve --host 0.0.0.0
