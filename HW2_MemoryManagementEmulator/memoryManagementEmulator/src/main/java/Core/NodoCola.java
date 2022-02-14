6f3e940f7bf04789cbb9bbe1776326e11a707871
KeylinVarela <anyelina.varela1995@gmail.com>
2022-02-14 00:25:01 -0600

    Interfaz
    cambios en Interfaz

---
+++ HW2_MemoryManagementEmulator/memoryManagementEmulator/src/main/java/Core/NodoCola.java
@@ -0,0 +1,15 @@
+
+package Core;
+
+
+public class NodoCola {
+    private int TaPartition;
+    public NodoCola SigPartition;
+
+    public NodoCola(int TaPartition) {
+        this.TaPartition = TaPartition;
+        this.SigPartition = SigPartition;
+    }
+    
+    
+}
