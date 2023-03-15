e5e84b59200b3011ed799afe3547d24a0aefb98b
Fernando0Murillo <86390468+Fernando0Murillo@users.noreply.github.com>
2023-03-15 01:58:45 -0600

    .

--- BD_SmartConsulting.sql
+++ BD_SmartConsulting.sql
@@ -12,30 +12,26 @@ CREATE TABLE servicios (
     UNIQUE KEY (nombre)
 );
 
+CREATE TABLE empleados (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    telefono VARCHAR(20),
+    fecha_contratacion DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE usuarios (
     id INT(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    rol ENUM('administrador', 'usuario') NOT NULL DEFAULT 'usuario',
+    id_empleado INT(11) NOT NULL,
+    rol ENUM('administrador', 'usuario','contador') NOT NULL DEFAULT 'usuario',
     PRIMARY KEY (id),
     UNIQUE KEY (username)
+    FOREIGN KEY (id_empleado) REFERENCES empleados(id)
 );
 
-CREATE TABLE citas (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    id_usuario INT(11) NOT NULL,
-    id_servicio INT(11) NOT NULL,
-    fecha DATE NOT NULL,
-    hora TIME NOT NULL,
-    duracion INT(11) NOT NULL,
-    estado ENUM('confirmada', 'cancelada', 'realizada') NOT NULL DEFAULT 'confirmada',
-    comentario TEXT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
-    FOREIGN KEY (id_servicio) REFERENCES servicios(id)
-);
-
-/*
 CREATE TABLE clientes (
     id INT(11) NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
@@ -45,7 +41,24 @@ CREATE TABLE clientes (
     preferencias TEXT,
     PRIMARY KEY (id)
 );
-*/
+
+
+CREATE TABLE citas (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    id_usuario INT(11) NOT NULL,
+    id_servicio INT(11) NOT NULL,
+    id_cliente Int(11) NOT NULL,
+    fecha DATE NOT NULL,
+    hora TIME NOT NULL,
+    duracion INT(11) NOT NULL,
+    estado ENUM('confirmada', 'cancelada', 'realizada') NOT NULL DEFAULT 'confirmada',
+    comentario TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
+    FOREIGN KEY (id_servicio) REFERENCES servicios(id)
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id)
+);
+
 
 CREATE TABLE pagos (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -66,7 +79,6 @@ CREATE TABLE empleados (
     email VARCHAR(50),
     telefono VARCHAR(20),
     fecha_contratacion DATE NOT NULL,
-    roles TEXT,
     PRIMARY KEY (id)
 );
 
@@ -88,3 +100,44 @@ CREATE TABLE inventario (
     descripcion TEXT,
     PRIMARY KEY (id)
 );
+
+
+-----------------
+/*Llenado general de tablas*/
+INSERT INTO empleados (nombre, email, telefono, fecha_contratacion)
+VALUES 
+('Juan Pérez', 'juan.perez@empresa.com', '9988-1234', '2022-01-01', 'contador'),
+('María García', 'maria.garcia@empresa.com', '9977-5678', '2022-01-02', 'usuario'),
+('Pedro González', 'pedro.gonzalez@empresa.com', '9966-9012', '2022-01-03', 'administrador');
+
+INSERT INTO horarios (id_empleado, dia_semana, hora_inicio, hora_fin)
+VALUES 
+(1, 'lunes', '09:00:00', '17:00:00'),
+(1, 'martes', '09:00:00', '17:00:00'),
+(1, 'miercoles', '09:00:00', '17:00:00'),
+(1, 'sabado', '10:00:00', '14:00:00'),
+(1, 'domingo', '10:00:00', '14:00:00'),
+
+(2, 'miercoles', '08:00:00', '16:00:00'),
+(2, 'jueves', '08:00:00', '16:00:00'),
+(2, 'viernes', '08:00:00', '16:00:00'),
+(2, 'sabado', '09:00:00', '13:00:00'),
+(2, 'domingo', '09:00:00', '13:00:00'),
+
+(3, 'lunes', '09:00:00', '17:00:00'),
+(3, 'martes', '09:00:00', '17:00:00'),
+(3, 'sabado', '10:00:00', '14:00:00'),
+(3, 'domingo', '10:00:00', '14:00:00');
+
+INSERT INTO inventario (nombre, cantidad, precio, descripcion)
+VALUES 
+('Tijeras de peluquería', 5, 150.00, 'Tijeras de acero inoxidable para cortar cabello.'),
+('Cepillo de pelo', 10, 25.00, 'Cepillo redondo de cerdas naturales para alisar el cabello.'),
+('Peine de cola', 7, 18.00, 'Peine de cola para seccionar el cabello y hacer peinados.'),
+('Pinzas para el cabello', 20, 5.00, 'Pinzas de plástico para sujetar el cabello durante el corte o peinado.'),
+('Capas de peluquería', 3, 20.00, 'Capas de nylon para proteger la ropa del cliente durante el corte de cabello.'),
+('Rulos para cabello', 15, 10.00, 'Rulos de plástico para crear ondas o rizos en el cabello.'),
+('Secador de pelo', 2, 1200.00, 'Secador de pelo profesional de alta potencia.'),
+('Plancha de pelo', 2, 2200.00, 'Plancha para alisar el cabello de cerámica y turmalina.'),
+('Tintes para cabello', 8, 250.00, 'Tintes semipermanentes para el cabello en diferentes tonos.'),
+('Permanentes para cabello', 5, 400.00, 'Líquidos para hacer permanentes en el cabello en diferentes niveles de ondulación.');
